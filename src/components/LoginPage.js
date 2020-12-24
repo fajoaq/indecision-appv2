3928c60041564610288e1cbe214db9a14a508cad
Francis J <fajoaq@gmail.com>
2020-12-23 19:28:17 -0500

    init commit

---
+++ src/components/LoginPage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { startLogin } from '../actions/auth';
+
+export const LoginPage = ({ startLogin }) => (
+    <div className="box-layout">
+        <div className="box-layout__box">
+            <h1 className="box-layout__title">Indecision App</h1>
+            <p>Let a.i make the decision for you!</p>
+            <button id="googleLogin" className="button" onClick={ startLogin }>Login with Google</button>
+        </div>
+        
+    </div>
+);
+
+const mapDispatchToProps = (dispatch) => ({
+    startLogin: () => dispatch(startLogin())
+});
+
+export default connect(undefined, mapDispatchToProps)(LoginPage);
